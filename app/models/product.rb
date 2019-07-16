842b8fd4f4efb50c876cca3a7675b23f064d1dc8
Kelvin Azaria <kelvin.azaria.billy@gmail.com>
2019-07-16 14:07:26 +0700

    Add Admin Functionality

---
+++ app/models/product.rb
@@ -0,0 +1,29 @@
+class Product < ApplicationRecord
+	belongs_to :supplier
+
+	RailsAdmin.config do |config|
+      config.model Product do
+        export do
+          field :id
+          field :name
+          field :price
+          field :supplier
+        end
+
+        edit do
+          field :name do
+            required true
+          end
+          field :price do
+            required true
+          end
+          field :desc do
+            required true
+          end
+          field :supplier do
+            required true
+          end
+        end
+      end
+    end
+end
