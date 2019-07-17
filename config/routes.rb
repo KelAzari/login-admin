5b6492e48112b158522977594b837d6147560fc8
Kelvin Azaria <kelvin.azaria.billy@gmail.com>
2019-07-17 10:57:33 +0700

    differentiate between admin and user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # devise_for :admins
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  root 'rails_admin/main#dashboard'
+  # root 'rails_admin/main#dashboard'
+  root 'home#index'
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
